07b218fdd4b26efea72150643c3b6d3c6cf8fdaa
LamerN <90185479+LamerN@users.noreply.github.com>
2022-04-01 01:08:56 +0300

    create Button method create OrderActivity

--- app/src/main/java/com/example/cafeorder/LoginActivity.java
+++ app/src/main/java/com/example/cafeorder/LoginActivity.java
@@ -2,13 +2,38 @@ package com.example.cafeorder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextTextPersonName;
+    private EditText editTextTextPassword;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        editTextTextPersonName = findViewById(R.id.editTextTextPersonName);
+        editTextTextPassword = findViewById(R.id.editTextTextPassword);
+    }
+
+
+    public void onClickCreateOrder(View view) {
+        String name = editTextTextPersonName.getText().toString().trim();
+        String password = editTextTextPassword.getText().toString().trim();
+        if (!name.isEmpty() && !password.isEmpty()){
+            Intent intent = new Intent(this, CreateOrderActivity.class);
+            intent.putExtra("name", name);
+            intent.putExtra("password", password);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, R.string.toast_fill_fields, Toast.LENGTH_SHORT).show();
+        }
+
     }
 }
